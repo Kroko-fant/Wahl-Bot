19a5546f40614568931e37aaf481c61f19cac017
Kroko-fant <schrottsuperdeluxe@gmail.com>
2019-11-01 14:48:58 +0100

    Version Beta 1.1.0

--- cogs/autoverify.py
+++ cogs/autoverify.py
@@ -14,12 +14,12 @@ class Autoverify(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def setmainrole(self, ctx, roleid):
-        with open('./data/mainrole.json', 'r') as f:
+        with open('./data/roles/mainrole.json', 'r') as f:
             roles = json.load(f)
 
         roles[str(ctx.guild.id)] = int(roleid)
 
-        with open('./data/mainrole.json', 'w') as f:
+        with open('./data/roles/mainrole.json', 'w') as f:
             json.dump(roles, f, indent=4)
         await bp.delete_cmd(ctx)
         await ctx.send("Main-Rolle gesetzt.", delete_after=bp.deltime)
@@ -27,12 +27,12 @@ class Autoverify(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def setspacerone(self, ctx, roleid):
-        with open('./data/spacerone.json', 'r') as f:
+        with open('./data/roles/spacerone.json', 'r') as f:
             roles = json.load(f)
 
         roles[str(ctx.guild.id)] = int(roleid)
 
-        with open('./data/spacerone.json', 'w') as f:
+        with open('./data/roles/spacerone.json', 'w') as f:
             json.dump(roles, f, indent=4)
         await bp.delete_cmd(ctx)
         await ctx.send("Spacer 1 -Rolle gesetzt.", delete_after=bp.deltime)
@@ -40,12 +40,25 @@ class Autoverify(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def setspacertwo(self, ctx, roleid):
-        with open('./data/spacertwo.json', 'r') as f:
+        with open('./data/roles/spacertwo.json', 'r') as f:
             roles = json.load(f)
 
         roles[str(ctx.guild.id)] = int(roleid)
 
-        with open('./data/spacertwo.json', 'w') as f:
+        with open('./data/roles/spacertwo.json', 'w') as f:
+            json.dump(roles, f, indent=4)
+        await bp.delete_cmd(ctx)
+        await ctx.send("Spacer 2 Rolle gesetzt.", delete_after=bp.deltime)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def setspacerthree(self, ctx, roleid):
+        with open('./data/roles/spacerthree.json', 'r') as f:
+            roles = json.load(f)
+
+        roles[str(ctx.guild.id)] = int(roleid)
+
+        with open('./data/roles/spacerthree.json', 'w') as f:
             json.dump(roles, f, indent=4)
         await bp.delete_cmd(ctx)
         await ctx.send("Spacer 2 Rolle gesetzt.", delete_after=bp.deltime)
@@ -57,19 +70,24 @@ class Autoverify(commands.Cog):
                 guild = message.guild
                 member = message.guild.get_member(int(message.author.id))
                 # Main Rolle setzen
-                with open('./data/mainrole.json', 'r') as f:
+                with open('./data/roles/mainrole.json', 'r') as f:
                     roles = json.load(f)
                 roleid = (roles[str(guild.id)])
                 role = guild.get_role(roleid)
                 await member.add_roles(role, reason="verify")
                 # Spacer 1 Setzen
-                with open('./data/spacerone.json', 'r') as f:
+                with open('./data/roles/spacerone.json', 'r') as f:
                     roles = json.load(f)
                 roleid = (roles[str(guild.id)])
                 role = guild.get_role(roleid)
                 await member.add_roles(role, reason="verify")
                 # Spacer 2 Setzen
-                with open('./data/spacertwo.json', 'r') as f:
+                with open('./data/roles/spacertwo.json', 'r') as f:
+                    roles = json.load(f)
+                roleid = (roles[str(guild.id)])
+                role = guild.get_role(roleid)
+                await member.add_roles(role, reason="verify")
+                with open('./data/roles/spacerthree.json', 'r') as f:
                     roles = json.load(f)
                 roleid = (roles[str(guild.id)])
                 role = guild.get_role(roleid)
