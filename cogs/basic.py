a545686384998aba4be1e8fc148b9b94990ccdab
Kroko-fant <schrottsuperdeluxe@gmail.com>
2019-11-11 21:25:57 +0100

    Version Beta 1.1.2

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,5 +1,3 @@
-import json
-
 import discord
 from discord.ext import commands
 
@@ -15,34 +13,40 @@ class Basic(commands.Cog):
     @commands.command()
     async def version(self, ctx):
         await bp.delete_cmd(ctx)
-        versiontext = "Der Bot läuft auf " + "Version Beta 1.1.1" + ". Die API läuft auf Version " + bp.apiversion()
+        versiontext = "Der Bot läuft auf " + "Version Beta 1.1.2" + ". Die API läuft auf Version " + bp.apiversion()
         await ctx.send(versiontext, delete_after=bp.deltime)
 
     @commands.command()
     async def bug(self, ctx, *, bugt):
+        channel = self.client.get_channel(int(643546804750909454))
+        content = "**Bug von " + str(ctx.author) + ":** " + bugt
+        await channel.send(content)
         await bp.delete_cmd(ctx)
-        with open('./botdata/bugs.json', 'r') as f:
-            bugs = json.load(f)
-        bugs[str(bugt)] = str(ctx.author.id)
-
-        with open('./botdata/bugs.json', 'w') as f:
-            json.dump(bugs, f, indent=4)
-
         await ctx.send('Danke <@' + str(ctx.author.id) + '> für das einreichen deines Bugs, wir melden uns zurück.',
                        delete_after=bp.deltime)
 
+    @bug.error
+    async def userinfo_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            errorba02embed = discord.Embed(title="Error #BA02",
+                                           description="Fehlendes Feedback! Syntax: bug <userid>", color=0xff0000)
+            await ctx.send(embed=errorba02embed)
+
     @commands.command()
     async def feedback(self, ctx, *, feedbackt):
+        channel = self.client.get_channel(int(643540415919816717))
+        content = "**Feedback von " + str(ctx.author) + ":** " + feedbackt
+        await channel.send(content)
         await bp.delete_cmd(ctx)
-        with open('./botdata/feedback.json', 'r') as f:
-            feedbacks = json.load(f)
-        feedbacks[str(feedbackt)] = str(ctx.author.id)
-
-        with open('./botdata/feedback.json', 'w') as f:
-            json.dump(feedbacks, f, indent=4)
-
         await ctx.send('Danke <@' + str(ctx.author.id) +
                        '> für das einreichen deines Feedbacks, wir melden uns zurück.', delete_after=bp.deltime)
+
+    @feedback.error
+    async def userinfo_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            errorba01embed = discord.Embed(title="Error #BA01",
+                                           description="Fehlendes Feedback! Syntax: feedback <userid>", color=0xff0000)
+            await ctx.send(embed=errorba01embed)
 
     @commands.command()
     async def about(self, ctx):
